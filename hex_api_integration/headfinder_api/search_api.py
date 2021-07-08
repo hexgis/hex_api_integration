6f6412ac7a2b33e00335c98ba3e3365adc4b6e79
Victor Ferreira <victormefer@gmail.com>
2021-07-08 17:45:01 -0300

    Implementing HEADFinder Search API

---
+++ hex_api_integration/headfinder_api/search_api.py
@@ -0,0 +1,237 @@
+import os
+import json
+import requests
+import tempfile
+import urllib.parse
+
+
+class Api():
+
+    def __init__(self, user_key: str):
+        """Api class for HEADFinder API.
+
+        Args:
+            user_key (str): user key acquired from HEAD
+        """
+        self.user_key = user_key
+
+    def get_api_url(self) -> str:
+        """Get HEADFinder Search API URL.
+
+        Returns:
+            str: HEADFinder Search API URL
+        """
+        return 'https://headfinder.head-aerospace.eu/search-ext-01/'
+
+    def get_payload(
+        self,
+        bbox: list = None,
+        geometry: list = None,
+        satellites: list = None,
+        scene_name: str = None,
+        scene_name_exact_match: bool = True,
+        max_scenes: int = 50,
+        start_date: str = None,
+        end_date: str = None,
+        cloud_cover: int = 100,
+        incidence_angle: int = 60,
+    ) -> dict:
+        """Get payload data object with user parameters.
+
+        Args:
+            bbox (list, optional):
+                Bounding box of AOI. Defaults to None.
+            geometry (list, optional):
+                AOI in WKT polygon format. Defaults to None.
+            satellites (list, optional):
+                List of HEAD satellites. Defaults to None.
+            scene_name (str, optional):
+                Scene name. Defaults to None.
+            scene_name_exact_match (bool, optional):
+                Match name exactly if true, partially if false.
+                Defaults to True.
+            max_scenes (int, optional):
+                Max number of scenes to return. Limited to 50. Defaults to 50.
+            start_date (str, optional):
+                Start date in YYYY-MM-DD format. Defaults to None.
+            end_date (str, optional):
+                End date in YYYY-MM-DD format. Defaults to None.
+            cloud_cover (int, optional):
+                Max percent cloud cover. Defaults to 100.
+            incidence_angle (int, optional):
+                Max percent of incidence angle. Defaults to 60.
+
+        Returns:
+            dict: payload data object
+        """
+        payload = {}
+        payload['req'] = 'd01'
+        payload['category'] = 'searchapi-01'
+        payload['user'] = self.user_key
+        payload['overlapmin'] = 10
+
+        if geometry:
+            payload['aoi'] = geometry
+        elif bbox:
+            payload['aoi'] = 'rectangle(({},{})({},{}))'.format(*bbox)
+
+        if satellites:
+            satellites = '$'.join(satellites)
+            payload['satellites'] = f'${satellites}$'
+
+        if scene_name:
+            payload['scenename'] = scene_name
+
+        if scene_name_exact_match:
+            payload['scenenamematch'] = 'exact'
+        else:
+            payload['scenenamematch'] = 'partial'
+
+        if max_scenes and max_scenes <= 50:
+            payload['maxscenes'] = max_scenes
+        else:
+            payload['maxscenes'] = 50
+
+        if start_date:
+            payload['datestart'] = start_date
+
+        if end_date:
+            payload['dateend'] = end_date
+
+        if cloud_cover:
+            payload['cloudmax'] = cloud_cover
+
+        if incidence_angle:
+            payload['offnadirmax'] = incidence_angle
+
+        return payload
+
+    def get_response_data(
+        self,
+        payload: dict
+    ) -> dict:
+        """Get data from search api url.
+
+        Requests data from HEADFinder Search API.
+        Uses filters from payload data request
+        and returns requests response object
+
+        Argus:
+            payload (dict): payload data for filtered request
+
+        Returns:
+            requests.Response: response data
+        """
+        payload = urllib.parse.urlencode(payload, safe=',$()')
+        response = requests.get(
+            self.get_api_url(),
+            params=payload
+        )
+
+        if response.ok:
+            if (response.text == 'bad request'):
+                raise AttributeError()
+            return self._handle_response_data(response.text)
+
+        return None
+
+    def _handle_response_data(
+        self,
+        data: str
+    ) -> dict:
+        data = data.split('&jsonscenelist=')[1]
+        data = data.split('&hits=')[0]
+        data = json.loads(data)
+
+        if len(data) > 0:
+            if (data[0] == 'dummy record 0'):
+                data.pop(0)
+                for scene in data:
+                    scene = self._handle_scene_footprint(scene)
+
+        return data
+
+    def _handle_scene_footprint(
+        self,
+        scene: dict
+    ) -> dict:
+        footprint = zip(scene['footprintlat'], scene['footprintlon'])
+        scene['footprint'] = [[x, y] for x, y in footprint]
+        scene['footprint'].pop(0)
+        del scene['footprintlat']
+        del scene['footprintlon']
+        return scene
+
+    def get_image_data(self, preview_url, img_size='LARGE'):
+        """
+        *Get image data blob from feature image_url*
+
+        Arguments:
+            * preview_url (str): preview url for request
+            * img_size (str): image size.
+                Could be: 'SMALL', 'MEDIUM', 'LARGE'
+
+        Returns:
+            * response (requests.response): a requests response data
+        """
+
+        headers = self._get_authenticated_headers_image()
+        payload = {'size': img_size}
+        response = requests.get(
+            preview_url,
+            headers=headers,
+            params=json.dumps(payload)
+        )
+
+        return response
+
+    def get_image_path(
+        self,
+        feature=None,
+        preview_url=None,
+        img_size='LARGE'
+    ):
+        """
+        *Get image path from feature image_url or preview_url*
+
+        Arguments:
+            * feature (dict): geojson feature data
+            * preview_url (str): preview url data
+            * img_size (str): image size.
+                Could be: 'SMALL', 'MEDIUM', 'LARGE'
+
+        Returns:
+            * path (str): path to image
+        """
+
+        error_msg = 'Both feature and preview_url is not allowed'
+
+        if feature and preview_url:
+            raise ValueError(error_msg)
+
+        if feature:
+            thumbnails = filter(
+                lambda x: x.get('size') == img_size,
+                feature.get('quicklooks')
+            )
+            thumbnails = next(thumbnails).get('image')
+            response = self.get_image_data(
+                preview_url=thumbnails, img_size=img_size)
+        else:
+            response = self.get_image_data(
+                preview_url=preview_url, img_size=img_size)
+
+        if response and response.ok:
+            temp_path = os.path.join(
+                tempfile._get_default_tempdir(),
+                next(tempfile._get_candidate_names()) + '.jpg'
+            )
+            try:
+                with open(temp_path, 'wb') as f:
+                    f.write(response.content)
+                    f.close()
+                return temp_path
+            except Exception as exc:
+                raise ValueError('Error while writing image: {}'.format(exc))
+
+        return None
---
+++ tests/test_headfinder_search_api.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+"""Tests for `hex_api_integration` package."""
+
+import os
+import pytest
+import sys
+
+from hex_api_integration.headfinder_api.search_api import Api as SearchApi
+
+if os.getenv('TEST_HEADFINDER_API_KEY') is None:
+    sys.exit('Please define env TEST_HEADFINDER_API_KEY for testing')
+
+
+@pytest.fixture
+def search_api():
+    """SearchAPI fixture data.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    return SearchApi(os.getenv('TEST_HEADFINDER_API_KEY'))
+
+
+@pytest.fixture
+def geom():
+    """Geometry fixture data.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    return ('polygon(((53.715,5.037),'
+            '(52.018,26.307),'
+            '(37.535,16.287),'
+            '(43.028,-1.290)))')
+
+
+# @pytest.fixture
+# def preview_url():
+#     """
+#     Preview Url fixture data.
+
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     return 'https://search.federated.Search-airbusds.com/' \
+#         'api/v1/productTypes/SPOTArchive1.5Mono/products/' \
+#         'DS_SPOT7_201801070929101_CB1_CB1_CB1_CB1_W021S78_01627' \
+#         '?size=LARGE'
+
+
+def test_get_search_api_url(search_api):
+    """Tests get geostore url private method for SearchAPI"""
+    search_api_url = search_api.get_api_url()
+    assert search_api_url
+    assert type(search_api_url) == str
+
+
+def test_get_payload_data_parameter(search_api):
+    """Tests get payload dict method for SearchAPI with parameter data"""
+    payload = search_api.get_payload(scene_name='TEST')
+    assert payload
+    assert type(payload) == dict
+    assert payload.get('scenename') == 'TEST'
+
+
+def test_get_response_data_parameter(search_api, geom):
+    """Tests get response data method for SearchAPI"""
+    payload = search_api.get_payload(
+        geometry=geom,
+        start_date='2020-07-15',
+        end_date='2020-08-01',
+        max_scenes=2,
+        satellites=['SuperView', 'EarthScanner-KF1'],
+    )
+    assert payload
+    assert type(payload) == dict
+    assert payload.get('aoi')
+    data = search_api.get_response_data(payload)
+    assert data
+    assert len(data)
+    assert data[0].get('identifier')
+    assert data[0].get('footprint')
+
+
+# def test_get_response_data_image_blob(search_api, preview_url):
+#     """Tests get response image blob data method for SearchAPI"""
+#     thumbs = search_api.get_image_data(preview_url)
+#     assert thumbs
+
+#     thumbs = search_api.get_image_data(preview_url + 'err')
+#     assert not thumbs
+
+
+# def test_get_response_data_image_path(search_api, preview_url):
+#     """Tests get response image path method for SearchAPI"""
+#     thumbs = search_api.get_image_path(
+#         preview_url=preview_url
+#     )
+#     assert os.path.exists(thumbs)
+
+#     thumbs = search_api.get_image_path(
+#         preview_url=preview_url + 'test'
+#     )
+#     assert not thumbs
+
+
+# def test_get_response_data_image_path_error(search_api, preview_url):
+#     """Tests get response image path for SearchAPI with ValueError"""
+#     with pytest.raises(ValueError):
+#         search_api.get_image_path(
+#             feature=True,
+#             preview_url=preview_url
+#         )
+
+
+# def test_get_response_data_image_path_feature(search_api, geom):
+#     """
+#     Tests get response image path for SearchAPI with path from feature
+#     """
+#     payload = search_api.get_payload(
+#         geometry=geom,
+#         acquisition_date_range=['2020-01-01', '2020-02-01'],
+#         constellation=['PLEIADES'],
+#         cloud_cover=10,
+#     )
+#     assert payload
+#     assert type(payload) == dict
+#     assert payload.get('acquisitionDate')
+
+#     response = search_api.get_response_data(payload)
+#     data = response.json()
+#     thumbs = search_api.get_image_path(
+#         feature=data.get('features')[0]
+#     )
+#     assert data
+#     assert thumbs
+#     assert response.status_code == 200

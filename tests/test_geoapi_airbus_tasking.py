950f0974d7cf326452a9b2f0c74ce56805aa76f5
dagnaldo <naldo.penha@gmail.com>
2022-12-26 18:30:48 -0300

    WIP for get_taskings.

---
+++ tests/test_geoapi_airbus_tasking.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""Tests for `hex_api_integration` package."""
+
+import os
+import pytest
+import sys
+
+from hex_api_integration.geoapi_airbus.tasking import Api as GeoApi
+
+if os.getenv('TEST_AIRBUS_API_KEY') is None:
+    sys.exit('Please set env TEST_AIRBUS_API_KEY for testing')
+
+
+@pytest.fixture
+def geo_api():
+    """GeoAPI fixture data.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    return GeoApi(os.getenv('TEST_AIRBUS_API_KEY'))
+
+
+def test_get_headers(geo_api):
+    """Tests get headers private method for GeoAPI."""
+
+    headers = geo_api._get_authenticated_headers()
+    assert headers
+    assert headers.get('Authorization')
+
+
+def test_get_cis_contracts_ids(geo_api):
+    """Tests get cis contract ids method for GeoAPI."""
+
+    cis_contract_ids = geo_api.get_cis_contract_ids()
+    assert cis_contract_ids
+    assert len(cis_contract_ids) > 0
+
+
+# def test_get_taskings(geo_api):
+#     """Tests get cis contract ids method for GeoAPI."""
+#     usage = geo_api.get_tasking_from_contracts_ids()
